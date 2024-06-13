96943b4fa42d77313e993e721a3cedbbeeabbb7a
Rafael Correia <rafael.correia@alloymit.com>
2024-06-13 19:19:49 -0300

    build and publish script

--- script/build-docker.sh
+++ script/build-docker.sh
@@ -1,3 +1,4 @@
+export DOCKER_HUB_ACCOUNT=fiapmyburguer
 export BUILD_DATE_FORMAT="+%Y%m%d%H%M"
 export BUILD_DATE=`date $BUILD_DATE_FORMAT`
 export CI_PROJECT_NAME="$(basename $PWD)"
@@ -5,6 +6,6 @@ export CI_COMMIT_BRANCH="latest"
 
 echo "building project=$CI_PROJECT_NAME nametag=$CI_COMMIT_BRANCH datetag=$BUILD_DATE"
 
-docker buildx build . -t $CI_PROJECT_NAME:$CI_COMMIT_BRANCH -t $CI_PROJECT_NAME:$BUILD_DATE
-#docker push -a $CI_PROJECT_NAME
+docker buildx build . -t $DOCKER_HUB_ACCOUNT/$CI_PROJECT_NAME:$CI_COMMIT_BRANCH -t $DOCKER_HUB_ACCOUNT/$CI_PROJECT_NAME:$BUILD_DATE
+docker push -a $DOCKER_HUB_ACCOUNT/$CI_PROJECT_NAME
 
