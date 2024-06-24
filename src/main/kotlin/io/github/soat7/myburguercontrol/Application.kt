59552c650893fa2cc859d2e2b7d6663e0cb756ee
Rafael Correia <rafaeljpc@gmail.com>
2024-06-24 01:40:26 -0300

    fixing myburguer to myburger

--- src/main/kotlin/io/github/soat7/myburguercontrol/Application.kt
+++ src/main/kotlin/io/github/soat7/myburguercontrol/Application.kt
@@ -2,7 +2,6 @@ package io.github.soat7.myburguercontrol
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 class Application
