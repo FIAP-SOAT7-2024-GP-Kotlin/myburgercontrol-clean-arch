6c85eb5bb3c5a848dfe8e655332274cacaa15158
Rafael Correia <rafael.correia@alloymit.com>
2024-07-15 14:18:13 -0300

    init db improve

--- script/k8s-cluster/init-db-on-cluster.sh
+++ script/k8s-cluster/init-db-on-cluster.sh
@@ -7,6 +7,10 @@ kubectl apply -f k8s/db/postgres-configmap.yaml
 kubectl apply -f k8s/db/postgres-deployment.yaml
 kubectl apply -f k8s/db/postgres-svc.yaml
 
+# wait db to up
+echo "Waiting to db goes up"
+sleep 20s
+
 # Initialize database
 # Create Role and Database
 cat script/db/my_burger_db_creation.sql |  kubectl exec deployments/postgres -it -- psql -U postgres -W -h localhost
