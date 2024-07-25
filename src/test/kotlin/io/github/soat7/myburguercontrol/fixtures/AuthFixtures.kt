b4aff2f4d4beb7ef850c0b65097209499b66b888
Rafael Correia <rafaeljpc@gmail.com>
2024-07-25 12:52:05 -0300

    lint

--- src/test/kotlin/io/github/soat7/myburguercontrol/fixtures/AuthFixtures.kt
+++ src/test/kotlin/io/github/soat7/myburguercontrol/fixtures/AuthFixtures.kt
@@ -2,7 +2,6 @@ package io.github.soat7.myburguercontrol.fixtures
 
 import io.github.soat7.myburguercontrol.external.webservice.auth.api.AuthRequest
 
-
 object AuthFixtures {
 
     fun mockAuthCreationRequest(cpf: String, password: String) = AuthRequest(
