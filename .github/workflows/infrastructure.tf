246436b66d6d51fe40663064c0ac902fe4cdd997
Andre Santos <alssantos.482@gmail.com>
2024-09-28 01:51:47 -0300

    Update deploy.yml

--- .github/workflows/infrastructure.tf
+++ .github/workflows/infrastructure.tf
@@ -2,11 +2,20 @@ provider "digitalocean" {
   token = var.digitalocean_token
 }
 
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"  # Especifique a versão adequada (por exemplo, 2.0 ou outra)
+    }
+  }
+}
+
 resource "digitalocean_droplet" "myburger" {
   image  = "docker-20-04"  # Escolha uma imagem com Docker instalado
-  name   = "myburgercontrol-droplet"
+  name   = "myburger-droplet"
   region = "nyc1"          # Você pode escolher a região
-  size   = "s-1vcpu-1gb"   # Escolha o tamanho do droplet
+  size   = "s-1vcpu-2gb"   # Escolha o tamanho do droplet
   ssh_keys = [var.ssh_fingerprint]
 }
 
