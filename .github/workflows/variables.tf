9fb19d07c1e2baf0d6ea2af236f76ba0e522eb88
Andre Santos <alssantos.482@gmail.com>
2024-09-28 02:33:50 -0300

    Update deploy.yml

--- .github/workflows/variables.tf
+++ .github/workflows/variables.tf
@@ -7,3 +7,8 @@ variable "ssh_fingerprint" {
   description = "Fingerprint da chave SSH para acessar o droplet"
   type        = string
 }
+
+variable "vpc_id" {
+  type    = string
+  default = "ba5f81a6-5df2-4149-ad14-10cb12440d95"
+}
